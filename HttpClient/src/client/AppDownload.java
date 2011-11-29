75e28676ada1e6f0438cbb41b88dd7f13b128ae6
alx.tremblay@gmail.com <alx.tremblay@gmail.com@75f7e6d0-9b50-07d7-63ee-ead52f89cdad>
2011-11-29 02:18:50 +0000

    Ajout du client :) TODO : Résoudre le problème avec les threads TODO : Faire une meilleur interface

---
+++ HttpClient/src/client/AppDownload.java
@@ -0,0 +1,75 @@
+package client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppDownload
+{
+	private List<DownloadThread> downloadsList;
+	
+	public AppDownload()
+	{
+		this.downloadsList = new ArrayList<DownloadThread>();
+	}
+	
+	public boolean addDownload(String url)
+	{
+		// TODO : Validation de l'url ?
+		DownloadThread dl = new DownloadThread(url);
+		return this.downloadsList.add(dl); 
+	}
+	
+	public void removeDownload(int pos)
+	{
+		if (this.downloadsList.size() > 0 && (pos >= 0 && pos <= this.downloadsList.size()))
+		{
+			if (this.downloadsList.get(pos).isAlive())
+			{
+				this.stopDownload(pos);
+			}
+			
+			this.downloadsList.remove(pos);
+		}
+	}
+	
+	public void moveUp(int pos)
+	{
+		if (pos > 0 && pos <= this.downloadsList.size())
+		{
+			DownloadThread temp = this.downloadsList.get(pos - 1);
+			this.downloadsList.set(pos - 1, this.downloadsList.get(pos));
+			this.downloadsList.set(pos, temp);
+		}
+	}
+	
+	public void moveDown(int pos)
+	{
+		if (pos >= 0 && pos < this.downloadsList.size())
+		{
+			DownloadThread temp = this.downloadsList.get(pos + 1);
+			this.downloadsList.set(pos + 1, this.downloadsList.get(pos));
+			this.downloadsList.set(pos, temp);
+		}
+	}
+	
+	public void startDownload(int pos)
+	{
+		if (pos >= 0 && pos <= this.downloadsList.size() && !this.downloadsList.get(pos).isAlive())
+		{
+			this.downloadsList.get(pos).start();
+		}
+	}
+	
+	public void stopDownload(int pos)
+	{
+		if (pos >= 0 && pos <= this.downloadsList.size()  && this.downloadsList.get(pos).isAlive())
+		{
+			this.downloadsList.get(pos).interrupt();
+		}
+	}
+	
+	public List<DownloadThread> getDownloadsList()
+	{
+		return this.downloadsList;
+	}
+}
