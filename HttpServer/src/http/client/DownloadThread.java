2bdfd1d30a28c6280f2b20faec051aa03844680e
alx.tremblay@gmail.com <alx.tremblay@gmail.com@75f7e6d0-9b50-07d7-63ee-ead52f89cdad>
2011-11-29 02:54:30 +0000

    Déplacement du client dans le nouveau paquet.

---
+++ HttpServer/src/http/client/DownloadThread.java
@@ -0,0 +1,67 @@
+package http.client;
+
+public class DownloadThread extends Thread
+{
+	private String path;
+	private boolean paused = false;
+	private boolean done = false;
+	
+	public DownloadThread(String path)
+	{
+		this.path = path;
+	}
+	
+	@Override
+	public void run()
+	{
+		// TODO : Faire quelque chose d'utile
+		while (!this.isDone())
+		{
+			synchronized (this)
+			{
+				while (this.paused)
+				{
+					try
+					{
+						this.wait();
+					}
+					catch (Exception e)
+					{
+					}
+				}
+				
+				System.out.println("[ " + this.path + " ] Downloading ...");
+			}
+		}
+	}
+	
+	public String getPath()
+	{
+		return this.path;
+	}
+	
+	public boolean isDone()
+	{
+		return this.done;
+	}
+	
+	public void pauseDownload()
+	{
+		this.paused = true;
+	}
+
+	public void resumeDownload()
+	{
+		this.paused = false;
+		
+		synchronized (this)
+		{
+			this.notifyAll();
+		}
+	}
+	
+	public boolean isPaused()
+	{
+		return this.paused;
+	}
+}
