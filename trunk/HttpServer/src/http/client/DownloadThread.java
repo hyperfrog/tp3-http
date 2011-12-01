5b69e57127d9b27f6821b78acb6c5228afd95271
alx.tremblay <alx.tremblay@75f7e6d0-9b50-07d7-63ee-ead52f89cdad>
2011-12-01 00:44:48 +0000

    Peut maintenant passer des requetes au serveur, mais le telechargement de fichier de semble pas fonctionner.

--- trunk/HttpServer/src/http/client/DownloadThread.java
+++ trunk/HttpServer/src/http/client/DownloadThread.java
@@ -1,43 +1,117 @@
 package http.client;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+import http.common.HttpRequest;
+import http.common.HttpResponse;
+
 public class DownloadThread extends Thread
 {
-	private String path;
+	private static final String USER_AGENT = "HttpClient Downloader";
+	private static final int KB_PER_SECOND = 50;
+	
+	private Socket socket = null;
+	private HttpRequest request;
+	private HttpResponse response;
+	
+	private String fileName;
+	
 	private boolean paused = false;
 	private boolean done = false;
 	
 	public DownloadThread(String path)
 	{
-		this.path = path;
+		try
+		{
+			URL url = new URL(path);
+			
+			// TODO : Temporaire
+			this.fileName = url.getPath().substring(url.getPath().lastIndexOf("/") + 1, url.getPath().length());
+			System.out.println(this.fileName);
+			
+			this.request = new HttpRequest();
+			this.request.getHeader().setMethod("GET");
+			this.request.getHeader().setFullPath(url.getFile());
+			this.request.getHeader().setProtocol("HTTP/1.1");
+			
+			this.request.getHeader().setField("Host", url.getHost());
+			this.request.getHeader().setField("User-Agent", DownloadThread.USER_AGENT);
+			this.request.getHeader().setField("Accept", "*/*");
+			
+			this.socket = new Socket(url.getHost(), (url.getPort() != -1) ? url.getPort() : url.getDefaultPort());
+		}
+		catch (MalformedURLException e)
+		{
+			e.printStackTrace();
+		}
+		catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public void run()
 	{
-		// TODO : Faire quelque chose d'utile
-		while (!this.isDone())
+		try
 		{
-			synchronized (this)
+			if (this.request.send(this.socket.getOutputStream()))
 			{
-				while (this.paused)
+				// TODO : Ne fonctionne pas ... et je ne comprends pas pourquoi ...
+				FileOutputStream fos = new FileOutputStream("C:\\Test\\" + this.fileName);
+					
+				byte[] buf = new byte[1024];
+				int len;
+
+				while ((len = this.socket.getInputStream().read(buf)) > 0)
 				{
+					System.out.println("Downloading...");
+
+					fos.write(buf, 0, len);
+
 					try
 					{
-						this.wait();
+						Thread.sleep(1000/KB_PER_SECOND);
 					}
-					catch (Exception e)
+					catch (InterruptedException e)
 					{
+						e.printStackTrace();
 					}
 				}
-				
-				System.out.println("[ " + this.path + " ] Downloading ...");
+
+				fos.close();
 			}
 		}
-	}
-	
-	public String getPath()
-	{
-		return this.path;
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+//		while (!this.isDone())
+//		{
+//			synchronized (this)
+//			{
+//				while (this.paused)
+//				{
+//					try
+//					{
+//						this.wait();
+//					}
+//					catch (Exception e)
+//					{
+//					}
+//				}
+//			}
+//		}
 	}
 	
 	public boolean isDone()
